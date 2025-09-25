43b7ce22c11848c5c88009f4e343531ffc9ec664
jovibfel@gmail.com <jovibfel@gmail.com>
2025-09-25 16:54:34 -0300

    Criação de tratamento de erros iniciais

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,22 @@
+package br.com.microservices.orchestrated.paymentservice.config.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice//Anotação permitindo um handler global para todos os controllers
+public class ExceptionGlobalHandler {
+
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?>handleValidationException(ValidationException validationException){
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage()));
+    }
+
+
+}
