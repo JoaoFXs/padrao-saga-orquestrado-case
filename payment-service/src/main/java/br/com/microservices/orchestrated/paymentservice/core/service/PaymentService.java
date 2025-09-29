36c02c370f885b1fd2edd026a46665eb027ad1d6
jovibfel@gmail.com <jovibfel@gmail.com>
2025-09-29 19:22:23 -0300

    Payment-Service - Criando lógica de realizar pagamento com sucesso

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -1,7 +1,11 @@
 package br.com.microservices.orchestrated.paymentservice.core.service;
 
+import br.com.microservices.orchestrated.paymentservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.History;
 import br.com.microservices.orchestrated.paymentservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
+import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
@@ -10,6 +14,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @Service
 @AllArgsConstructor
@@ -17,6 +23,7 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MIN_AMOUNT_VALUE = 0.1;
     private final JsonUtil jsonUtil;
     private final KafkaProducer producer;
     private final PaymentRepository paymentRepository;
@@ -26,6 +33,10 @@ public class PaymentService {
         try{
             checkCurrentValidation(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception e) {
                log.error("Error Trying to make payment: ", e);
         }
@@ -79,6 +90,50 @@ public class PaymentService {
     private void setEventAmountItems(Event event, Payment payment){
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+
+    private void validateAmount(double totalAmount){
+        if(totalAmount < MIN_AMOUNT_VALUE){
+            throw new ValidationException("The minimum amount available is ".concat(MIN_AMOUNT_VALUE.toString()));
+        }
+    }
+
+    private void changePaymentToSuccess(Payment payment){
+        payment.setStatus(EPaymentStatus.SUCCESS);
+        save(payment);
+    }
+    private Payment findByOrderIdAndTransactionId(Event event){
+        return paymentRepository.findByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())
+                .orElseThrow(() -> new ValidationException("Payment not found by OrderID and TransactionID"));
+    }
+
+    /**
+     * Método para lidar com sucesso, salvando status como success e atribuindo source com current_source.
+     * Por mim chama addHistory para adicionar um historico ao evento
+     * @param event
+     */
+    private void handleSuccess(Event event){
+        event.setStatus(ESagaStatus.SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        /** Utilitario handleSuccess 1 **/
+        addHistory(event, "Payment realized with successlly!");
+    }
+
+    /**
+     * Utilitario handleSuccess 1 - Cria historico novo e adiciona na lista de historicos
+     *
+     * @param event
+     * @param message
+     */
+    private void addHistory(Event event, String message){
+        var history = History.builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
     }
 
 }
