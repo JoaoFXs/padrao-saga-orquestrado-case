bd17a2933ed198263138bf4bb3bcd9571eb1dfeb
jovibfel@gmail.com <jovibfel@gmail.com>
2025-09-30 18:41:25 -0300

    Inventory-Service - Modelando as classes e entidades de banco de dados

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -0,0 +1,60 @@
+package br.com.microservices.orchestrated.inventoryservice.core.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orderInventory")
+public class OrderInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", nullable = false)
+    private Inventory inventory;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private Integer orderQuantity;
+
+    @Column(nullable = false)
+    private Integer oldQuantity;
+
+    /** Essa coluna será a subtração da orderQuantity pela oldQuantity **/
+    @Column(nullable = false)
+    private Integer newQuantity;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    private void prePersist(){
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    private void preUpdated(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
